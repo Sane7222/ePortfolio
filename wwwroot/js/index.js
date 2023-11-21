9bf360b777d99870c5db4841ca90f69296c5a931
Sane <matias.moseley@gmail.com>
2023-11-20 19:30:47 -0800

    Home page implemented

--- wwwroot/js/index.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('h3').on('click', function () {
-        $(this).next().slideToggle(1000, function () { });
-    });
-});
-
-$(window).on('beforeunload', function () {
-
-});
